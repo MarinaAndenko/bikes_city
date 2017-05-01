9dd3977a9a8c468012bb67c36a71e7f53fe54391
Marina <marina.andenko@gmail.com>
2017-05-01 14:28:49 +0300

    add resource routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   get 'hello_world', to: 'hello_world#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :bikes
+  resources :addresses
+  resources :locations
+  resources :payments
+  resources :rentals
+  resources :tariffs
+  resources :user_tariffs
 end
